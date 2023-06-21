cfcad8fd556823f64f1f381c996e4b609a32d001
Peter <101114264+JustPeter64@users.noreply.github.com>
2023-06-21 09:43:23 +0200

    opruimen

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            // QuizTableSeeder::class,
             TagTableSeeder::class,
             AdminUserSeeder::class,
         ]);
